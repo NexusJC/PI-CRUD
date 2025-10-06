c9fed5ac0e0381305737b2be60b12f5132094ba4
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-10-06 09:30:31 -0600

    cambios ne backend para el servidor

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,35 @@
-// server.js
 const express = require('express');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 const app = express();
 
-// Cargar las variables de entorno
 dotenv.config();
 
-// Middleware para manejar solicitudes JSON
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectarse a la base de datos:', err);
+    return;
+  }
+  console.log('Conectado a la base de datos. ');
+});
+  
 app.use(express.json());
 
-// Rutas de autenticación
-const authRoutes = require('./routes/auth.routes.js');
-app.use('/auth', authRoutes);
-
-// Ruta básica de prueba
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// Configuración del puerto
+const authRoutes = require('./routes/auth.routes.js');
+app.use('/auth', authRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Servidor corriendo en puerto: ${PORT}`);
 });
