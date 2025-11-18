2c976be1f127285ff64979f7f29e25b38ac8618f
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-11-18 17:17:11 -0600

    se cambió la manera en la que se exportan lso datos al servidor

--- backend/routes/dishes.route.js
+++ backend/routes/dishes.route.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const { saveDish, getAllDishes } = require('../controllers/dishes.controller');
 const upload = require('../middlewares/upload.middleware');
 
-// POST /api/dishes
 router.post('/', upload.single('image'), saveDish);
 
-// GET /api/dishes
 router.get('/', getAllDishes);
 
-module.exports = router;
+export default router;
+
