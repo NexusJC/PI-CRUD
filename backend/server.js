79ef2e9f75bc6de833b705f2f44049a6822f8727
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-11-27 20:12:29 -0600

    cambios grandes hechos por ia como prueba

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["https://laparrilaazteca.online"],
+    origin: ["https://www.laparrilaazteca.online"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
