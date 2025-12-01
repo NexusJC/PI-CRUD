c0040c77fdfa7fa2606f034a99167d25607676a5
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-12-01 02:13:18 -0600

    cambios en los controladores para el guardado de imagenes

--- backend/routes/dishes.route.js
+++ backend/routes/dishes.route.js
@@ -5,7 +5,6 @@ import { upload } from '../config/upload.js';
 const router = express.Router();
 
 router.post('/', upload.single('image'), saveDish);
-router.post('/', upload.single('image'), saveDish);
 router.get('/', getAllDishes);
 router.delete('/:id', deleteDish);
 
