d2367bc10738846090be1cd70ae2135d8bb3c22f
Sahid <sahid2barroso@gmail.com>
2025-12-03 14:36:44 -0600

    probando

--- frontend/menu/js/sidebar.js
+++ frontend/menu/js/sidebar.js
@@ -116,11 +116,12 @@ document.addEventListener("DOMContentLoaded", () => {
       `;
     } else if (role === "usuario") {
       menuList.innerHTML = `  
-        <li data-no-translate>
-          <a href="/menu/index.html" data-no-translate>
-            <i class="fas fa-utensils" data-no-translate></i> Ver Menú
-          </a>
-        </li>
+        <li>
+  <a href="/menu/index.html">
+    <i class="fas fa-utensils" data-no-translate></i>
+    <span>Ver Menú</span>
+  </a>
+</li>
         <li data-no-translate>
           <a href="/perfil/perfil.html" data-no-translate>
             <i class="fas fa-user" data-no-translate></i> Mi Perfil
