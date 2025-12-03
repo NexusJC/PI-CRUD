bf381045628153a06bfc04ced5f0a71175afcde3
Sahid <sahid2barroso@gmail.com>
2025-12-03 14:29:08 -0600

    probando

--- frontend/menu/js/sidebar.js
+++ frontend/menu/js/sidebar.js
@@ -116,9 +116,21 @@ document.addEventListener("DOMContentLoaded", () => {
       `;
     } else if (role === "usuario") {
       menuList.innerHTML = `  
-        <li data-no-translate><a href="/menu/index.html"><i class="fas fa-utensils"data-no-translate></i> Ver Menú</a></li>
-        <li data-no-translate><a href="/perfil/perfil.html"><i class="fas fa-user"data-no-translate></i> Mi Perfil</a></li>
-        <li data-no-translate><a href="/shifts/shifts.html"><i class="fas fa-clock icon"data-no-translate></i> Turnos</a></li>
+        <li data-no-translate>
+          <a href="/menu/index.html" data-no-translate>
+            <i class="fas fa-utensils" data-no-translate></i> Ver Menú
+          </a>
+        </li>
+        <li data-no-translate>
+          <a href="/perfil/perfil.html" data-no-translate>
+            <i class="fas fa-user" data-no-translate></i> Mi Perfil
+          </a>
+        </li>
+        <li data-no-translate>
+          <a href="/shifts/shifts.html" data-no-translate>
+            <i class="fas fa-clock icon" data-no-translate></i> Turnos
+          </a>
+        </li>
       `;
     }
   }
