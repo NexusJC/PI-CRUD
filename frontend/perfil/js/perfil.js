f5d8af1e280785b19c91deb597cc5b136a31055c
itsbuglover <newthoney3@gmail.com>
2025-12-04 23:56:10 -0600

    pueeba de editar y guardar en perfil

--- frontend/perfil/js/perfil.js
+++ frontend/perfil/js/perfil.js
@@ -4,7 +4,7 @@
   const nombre = document.getElementById('perfilNombreText').value;
   const telefono = document.getElementById('perfilNumeroText').value;
   
-  const response = await fetch("https://www.laparrilaazteca.online/api/profile/get-profile", {
+  const response = await fetch("https://www.laparrilaazteca.online/api/profile/update-profile", {
     method: 'PUT',
     headers: {
       'Authorization': `Bearer ${localStorage.getItem('token')}`,
