8c55f854c5a298cb721d86c84dc622d751b39208
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-12-05 07:58:12 -0600

    ahora se validan las dos contraseñas antes de hacer registro

--- frontend/login/js/register.js
+++ frontend/login/js/register.js
@@ -2,7 +2,7 @@ function showToast(message, type = "error") {
   const toast = document.getElementById("toast");
   toast.textContent = message;
 
-  toast.className = "toast"; // reset classes
+  toast.className = "toast";
   toast.classList.add("show");
 
   if (type === "success") {
@@ -22,11 +22,23 @@ const form = document.getElementById("registerForm");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const data = {
-    name: form.name.value,
-    email: form.email.value,
-    password: form.password.value
-  };
+  const name = form.name.value;
+  const email = form.email.value;
+  const password = form.password.value;
+  const confirm = form.password2.value; 
+
+  //valida si son iguales
+  if (password !== confirm) {
+    showToast("Las contraseñas no coinciden.", "error");
+    return; 
+  }
+
+  if (password.length < 6) {
+    showToast("La contraseña debe tener mínimo 6 caracteres.", "error");
+    return;
+  }
+
+  const data = { name, email, password };
 
   try {
     const resp = await fetch("https://www.laparrilaazteca.online/api/auth/register", {
@@ -34,15 +46,10 @@ form.addEventListener("submit", async (e) => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     });
-
-    
     const result = await resp.json();
 
     if (resp.ok) {
       showToast("Registro exitoso. Revisa tu correo para confirmar tu cuenta.", "success");
-      // opcional: regresar al login
-      // document.getElementById("registerFormContainer").style.display = "none";
-      // document.getElementById("loginForm").style.display = "block";
     } else {
       showToast(result.error || result.message || "No se pudo registrar.", "error");
     }
