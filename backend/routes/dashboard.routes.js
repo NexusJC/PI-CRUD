cb2c1b37c5699d6c5c2fc341884714ffab9ee448
Sebastian <mhermosillo1@ucol.mx>
2025-12-06 15:40:54 -0600

    Error de graficas corregido

--- backend/routes/dashboard.routes.js
+++ backend/routes/dashboard.routes.js
@@ -11,21 +11,21 @@ router.get("/stats", getDashboardStats);
 router.get("/top-dishes", async (req, res) => {
     try {
         const [rows] = await pool.query(`
-            SELECT d.name AS dish,
-                   SUM(od.quantity) AS total_sold
+            SELECT 
+                od.dish_name AS dish,
+                SUM(od.quantity) AS total_sold
             FROM order_details od
-            INNER JOIN dishes d ON d.id = od.dish_id
-            GROUP BY od.dish_id
+            GROUP BY od.dish_name
             ORDER BY total_sold DESC
             LIMIT 5;
         `);
 
         res.json(rows);
-
     } catch (error) {
         console.error("Error al obtener platillos más vendidos:", error);
         res.status(500).json({ error: "Error al obtener datos" });
     }
 });
+
 
 export default router;
