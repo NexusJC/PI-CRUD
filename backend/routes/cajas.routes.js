dccb8d3715ada75c8725fcc870b6f39218bb28a9
NexusJC <jacobocisnerosbrandomyair@gmail.com>
2025-12-06 11:50:40 -0600

    el gestion cajas se hizo funcional

--- backend/routes/cajas.routes.js
+++ backend/routes/cajas.routes.js
@@ -1,15 +1,10 @@
-import express from "express";
+import { Router } from "express";
 import { getCajas, createCaja, deleteCaja } from "../controllers/cajas.controller.js";
 
-const router = express.Router();
+const router = Router();
 
-// Obtener todas las cajas
 router.get("/cajas", getCajas);
-
-// Crear una nueva caja
 router.post("/cajas", createCaja);
-
-// Eliminar una caja por ID
 router.delete("/cajas/:id", deleteCaja);
 
 export default router;
